75241b6267dbcb6102d6993b4cfa19b746bcd556
alice <rainingilove@gmail.com>
2018-06-27 22:05:05 +0200

    remove call after greeting

--- test/greeter.js
+++ test/greeter.js
@@ -3,7 +3,7 @@ var Greeter = artifacts.require('Greeter');
 contract('Greeter', function () {
   it('greeting with name', function () {
     return Greeter.deployed().then(function (instance) {
-      return instance.greeting.call('World');
+      return instance.greeting('World');
     }).then(function (greetingWords) {
       assert.equal(greetingWords, 'Hello World', '');
     });

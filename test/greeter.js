70a4ce909049eefea58b8108fa88ce844375ab1b
alice <rainingilove@gmail.com>
2018-06-27 22:11:38 +0200

    add call

--- test/greeter.js
+++ test/greeter.js
@@ -3,7 +3,7 @@ var Greeter = artifacts.require('Greeter');
 contract('Greeter', function () {
   it('greeting with name', function () {
     return Greeter.deployed().then(function (instance) {
-      return instance.greeting('World');
+      return instance.greeting.call('World');
     }).then(function (greetingWords) {
       assert.equal(greetingWords, 'Hello World', '');
     });

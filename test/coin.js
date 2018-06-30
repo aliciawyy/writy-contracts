6b14af0853a9f2ac375c28cb14e1dccc24c3422e
alice <rainingilove@gmail.com>
2018-06-30 22:09:08 +0200

    run lint fix

--- test/coin.js
+++ test/coin.js
@@ -4,7 +4,7 @@ contract('Coin', function (accounts) {
   var owner = accounts[0];
 
   it('minter address is owner', function () {
-    return Coin.deployed({'from': owner}).then(function (instance) {
+    return Coin.deployed({ 'from': owner }).then(function (instance) {
       return instance.minter.call();
     }).then(function (minter) {
       assert.equal(minter, owner);
